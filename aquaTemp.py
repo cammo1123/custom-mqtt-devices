c58d335203353375a06b7a10fdaeb5169cc32f4d
Cameron Youell <cameronyouell@gmail.com>
2021-11-24 14:58:52 +1100

    Clean and Reimplement mode switching

--- aquaTemp.py
+++ aquaTemp.py
@@ -57,40 +57,29 @@ class Heater:
 	
 	def update(self):	
 		url = "http://cloud.linked-go.com:84/cloudservice/api/app/device/getDataByCode.json"
+		myobj = {
 			"device_code": device_code,
-		# "Manual-mute", 
-			"T02", 		# In Temp
-			"2074",		# Flow Sensor
-		# "2075",
-		# "2076",
-		# "2077",
-		# "H03",
-			"Set_Temp",	# Target Temp
-		# "R08",
-		# "R09",
-		# "R10",
-		# "R11",
-		# "R01",
-		# "R02",
-		# "R03",		# Mirror Target Temp
-		# "T03", 		# Out Temp
-		# "1158",
-		# "1159",
-		# "F17",
-		# "H02",
-		# "2064"
-	   ]}
-		x = requests.post(url, json = myobj, headers = self.headers)
+			"protocal_codes": [        
+				"Power", 	# Power
+				"Mode", 	# Mode Temp
+				"T02", 		# In Temp
+				"T05",		# Ambient Temp
+				"2074",		# Flow Sensor
+				"Set_Temp",	# Target Temp
+			],
+	   }
+
+		try:
+			x = requests.post(url, json = myobj, headers = self.headers)
+		except ConnectionError:
+			print(f"{filename} - ConnectionError")
 
 		if (x.status_code == 200):
 			res = json.loads(x.text)
 
-			self.mode = self.modes[int([item for item in res["object_result"] if item.get('code') == "Mode"][0]["value"])]
+			self.mode = int([item for item in res["object_result"] if item.get('code') == "Mode"][0]["value"])
 			self.power = int([item for item in res["object_result"] if item.get('code') == "Power"][0]["value"])
 			self.flow = int([item for item in res["object_result"] if item.get('code') == "2074"][0]["value"], 2)
-
-			if (self.power == 0 or self.flow != 0):
-				self.mode = "off"
 
 			self.temp = float([item for item in res["object_result"] if item.get('code') == "T02"][0]["value"])
 			self.set_temp = float([item for item in res["object_result"] if item.get('code') == "Set_Temp"][0]["value"])
@@ -105,12 +94,19 @@ class Heater:
 			print(x.status_code)
 
 	def update_status(self):
+		if (self.power == 0 or self.flow != 0):
+			mode = "off"
+		else:
+			mode = self.modes[self.mode]
+
 		status = {
-			"mode": self.mode,
+			"mode": mode,
 			"temp": self.temp,
 			"set_temp": self.set_temp
 		}
+
 		self.client.publish(f"homeassistant/climate/{self.channel}/status", json.dumps(status))
+		self.client.publish(f"homeassistant/sensor/{self.channel}/temperature", self.ambient_temp)
 
 
 	def on_message(self, client, userdata, mess):
@@ -120,8 +116,10 @@ class Heater:
 			if "set_temp" in message:
 				self.set_temp = float(message["set_temp"])
 			if "mode" in message:
-				self.mode = message["mode"]
-				if (self.mode != "off"):
+				new_mode = self.modes.index(message["mode"])
+				
+				if (new_mode != 3):
+					self.mode = new_mode
 					self.power = 1
 				else:
 					self.power = 0
@@ -143,10 +141,23 @@ class Heater:
 					"protocol_code": "Set_Temp",
 					"value": f"{int(float(self.set_temp))}"
 				},
+				{
+					"device_code": device_code,
+					"protocol_code": "Mode",
+					"value": f"{int(float(self.mode))}"
+				},
+				{
+					"device_code": device_code,
+					"protocol_code": "Power",
+					"value": f"{int(float(self.power))}"
+				},
 			]
 		}
 
-		x = requests.post(url, json = json, headers = self.headers)
+		try:
+			x = requests.post(url, json = json, headers = self.headers)
+		except ConnectionError:
+			print(f"{filename} - ConnectionError")
 
 
 	def log(self):
